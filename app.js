fe73b6d5a4721e48b974b1a1b356c74ec6ed87d4
Kaan Ince <kaaniince@outlook.com>
2024-11-25 19:38:43 +0300

    partials added

--- app.js
+++ app.js
@@ -2,10 +2,26 @@ const express = require("express");
 
 const app = express();
 
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(express.static("public"));
+
 const PORT = 3000;
 
+//Routes
+
+app.use("/about", (req, res) => {
+  res.status(200).render("about", {
+    page_name: "about",
+  });
+});
+
 app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+  res.status(200).render("index", {
+    page_name: "index",
+  });
 });
 
 app.listen(PORT, () => {
