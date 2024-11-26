3a078a1e264c900edc3decd111b7123cd900061a
Kaan Ince <kaaniince@outlook.com>
2024-11-26 17:42:09 +0300

    added login and signup

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+exports.createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({
+      status: "success",
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).send("User Not Found!");
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).send("Invalid Password");
+    }
+    res.status(200).send("You are logged in!");
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      status: "failed",
+      err,
+    });
+  }
+};
